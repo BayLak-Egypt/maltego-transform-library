4a75c06eba037cb4cdd02f9ab2b6dc9ee4b5afd2
BayLak-Egypt <mh1231231234go@gmail.com>
2026-03-07 22:20:36 +0200

    library transforms

--- library/Sherlock/sherlock.py
+++ library/Sherlock/sherlock.py
@@ -1,10 +1,11 @@
 import subprocess
 import re
 import sys
+import os
 Name = 'Sherlock OSINT'
 Description = 'Realtime URLs from Sherlock scan'
 EntityType = 'Phrase'
-url_regex = re.compile('https?://[^\\s\\"\'>]+')
+url_regex = re.compile('https?://[^\\s\\"\\\'><]+')
 
 def send_debug(msg):
     sys.stderr.write(f'[DEBUG] {msg}\n')
@@ -15,6 +16,7 @@ def run(input_value):
     send_debug(f'Starting Sherlock scan for {username}')
     results = []
     unique_links = set()
+    filename = f'{username}.txt'
     try:
         process = subprocess.Popen(['sherlock', username, '--timeout', '5', '--print-found', '--no-color'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         for line in process.stdout:
@@ -27,7 +29,17 @@ def run(input_value):
                 if clean_url not in unique_links:
                     unique_links.add(clean_url)
                     results.append({'value': clean_url, 'type': 'URL', 'properties': {'url': clean_url, 'url.source': 'Sherlock Scan'}})
+        process.wait()
     except Exception as e:
-        send_debug(f'Error: {str(e)}')
+        send_debug(f'Error during execution: {str(e)}')
         results.append({'value': f'Error: {str(e)}', 'type': 'Phrase'})
+    finally:
+        if os.path.exists(filename):
+            try:
+                os.remove(filename)
+                send_debug(f'Deleted temporary file: {filename}')
+            except Exception as e:
+                send_debug(f'Could not delete file: {str(e)}')
+        else:
+            send_debug(f'No file found to delete for {username}')
     return results
