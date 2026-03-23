c7212bb116b0231ef8234f954d2a37b5cb6ba599
BayLak-Egypt <mh1231231234go@gmail.com>
2026-03-23 10:27:35 +0200

    library python

--- lib/loading.py
+++ lib/loading.py
@@ -1,11 +1,15 @@
+import os
+from lib.upgrade import get_remote_files, sync_file
 from loading_terminal import run_terminal_loader
 from loading_gui import start_gui_loader
 
 def start_loader(tasks, mode='gui'):
+    remote_files = get_remote_files()
+    sync_tasks_names = [f'Update: {os.path.basename(f['path'])}' for f in remote_files]
+    final_tasks = sync_tasks_names + tasks
+    for item in remote_files:
+        sync_file(item)
     if mode == 'gui':
-        start_gui_loader(tasks)
+        start_gui_loader(final_tasks)
     else:
-        run_terminal_loader(tasks)
-if __name__ == '__main__':
-    my_tasks = ['Database', 'Modules', 'API Keys', 'Finalizing']
-    start_loader(my_tasks, mode='gui')
+        run_terminal_loader(final_tasks)
--- lib/upgrade.py
+++ lib/upgrade.py
@@ -1,12 +1,37 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from processor import MaltegoGUILoader, run_terminal_loader
-my_tasks = ['Initializing Transform Engine...', 'Connecting to Maltego Graph Server...', 'Fetching WHOIS Data...', 'Finalizing Graph View...']
-mode = 'gui'
-if mode == 'gui':
-    app = QApplication(sys.argv)
-    loader = MaltegoGUILoader(tasks=my_tasks)
-    loader.show()
-    sys.exit(app.exec())
-else:
-    run_terminal_loader(tasks=my_tasks)
+import os
+import json
+import urllib.request
+import hashlib
+USER = 'BayLak-Egypt'
+REPO = 'maltego-transform-library'
+BRANCH = 'main'
+
+def get_remote_files():
+    api_url = f'https://api.github.com/repos/{USER}/{REPO}/git/trees/{BRANCH}?recursive=1'
+    try:
+        with urllib.request.urlopen(api_url, timeout=5) as r:
+            data = json.loads(r.read().decode())
+            return [f for f in data.get('tree', []) if f['type'] == 'blob']
+    except Exception:
+        return []
+
+def sync_file(item):
+    raw_url = f'https://raw.githubusercontent.com/{USER}/{REPO}/{BRANCH}/{item['path']}'
+    path = item['path']
+    if os.path.dirname(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    update = True
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            content = f.read()
+            header = f'blob {len(content)}\x00'.encode()
+            local_sha = hashlib.sha1(header + content).hexdigest()
+        if local_sha == item['sha']:
+            update = False
+    if update:
+        try:
+            urllib.request.urlretrieve(raw_url, path)
+            return True
+        except:
+            return False
+    return True
