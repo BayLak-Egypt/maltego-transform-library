e3018d2ed0817756802bae3710624319cc152477
BayLak-Egypt <mh1231231234go@gmail.com>
2026-03-23 10:39:22 +0200

    library python

--- lib/loading.py
+++ lib/loading.py
@@ -1,20 +1,26 @@
 import os
+import sys
 from lib.upgrade import get_remote_files, sync_file
 from loading_terminal import run_terminal_loader
 from loading_gui import start_gui_loader
 
 def start_loader(tasks, mode='gui'):
     remote_files = get_remote_files()
-    updated_files_tasks = []
+    updated_files = []
     for item in remote_files:
         if sync_file(item):
-            filename = os.path.basename(item['path'])
-            updated_files_tasks.append(f'Updated: {filename}')
-    if not updated_files_tasks:
-        final_tasks = ['System: Already up to date'] + tasks
+            updated_files.append(os.path.basename(item['path']))
+    if updated_files:
+        final_tasks = [f'Critical Update: {f}' for f in updated_files] + ['Restarting System...']
+        if mode == 'gui':
+            start_gui_loader(final_tasks)
+        else:
+            run_terminal_loader(final_tasks)
+        print('\n [!] System updated. Restarting process...')
+        os.execv(sys.executable, ['python'] + sys.argv)
     else:
-        final_tasks = updated_files_tasks + tasks
-    if mode == 'gui':
-        start_gui_loader(final_tasks)
-    else:
-        run_terminal_loader(final_tasks)
+        final_tasks = ['System: Up to date'] + tasks
+        if mode == 'gui':
+            start_gui_loader(final_tasks)
+        else:
+            run_terminal_loader(final_tasks)
